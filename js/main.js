1ab7c235f832986439ae6983c6369c9bcd8339e6
carlosbarrazzza <carlosbarrazzza15@gmail.com>
2024-08-06 19:58:12 -0300

    Actualizacion de mi html, css y nueva funcion de js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
-function iniciarRecomendador() {
-    let nombre = prompt("¡Que onda che! Bienvenido a este test sobre de que podrias comer hoy, te voy a guiar a la cocina con unos platillos excelentes. Por favor, ingresa tu nombre:");
-    
-    if (nombre === null || nombre === '') {
-        alert("Por favor, ingresa un nombre válido para continuar.");
+let carrito = [];
+let totalPrecio = 0;
+
+function agregarAlCarritoPa(nombreProducto, precioProducto) {
+    const productoExistente = carrito.find(producto => producto.nombre === nombreProducto);
+    if (productoExistente) {
+        productoExistente.cantidad++;
+    } else {
+        carrito.push({ nombre: nombreProducto, precio: precioProducto, cantidad: 1 });
+    }
+    totalPrecio += precioProducto;
+    document.getElementById('totalprecio').textContent = totalPrecio;
+    alert(`Has agregado ${nombreProducto} al carrito. Total: $${totalPrecio}`);
+}
+
+function mostrarCarrito() {
+    if (carrito.length === 0) {
+        alert("El carrito está vacío.");
         return;
     }
-    
-    alert("Hola, " + nombre + " " + "te voy a ayudar a elegir el plato del dia.");
-    
-    let preferencia = prompt("¿Que preferís comer hoy? Por favor, elige una opción:\n1. Carne\n2. Pollo\n3. Comida vegetariana");
-    
-    let platoRecomendado;
-    switch (preferencia) {
-        case '1':
-            platoRecomendado = 'Milangas con pastas (obviamente las pastas tienen que ir con crema y queso)';
-            break;
-        case '2':
-            platoRecomendado = 'Pollo al horno con papas (tenes que ponerle vino siosi al pollo)';
-            break;
-        case '3':
-            platoRecomendado = 'Tortilla de acelga (UN MANJAR)';
-            break;
-        default:
-            alert('Opcion no válida. Por favor, tenes que elegir una opción del 1 al 3.');
-            return;
-    }
-    
-    alert("Perfecto" + " " + nombre + " " + "te recomiendo hacer este plato el dia de hoy: " + platoRecomendado + " " + "(espero que lo hagas hoy eh)");
+    let mensajeCarrito = "Productos en el carrito:\n";
+    carrito.forEach(producto => {
+        mensajeCarrito += `${producto.nombre} - Cantidad: ${producto.cantidad} - Precio por unidad: $${producto.precio}\n`;
+    });
+    mensajeCarrito += `\nTotal: $${totalPrecio}`;
+    alert(mensajeCarrito);
 }
+
+document.getElementById('mostrarCarritoBtn').onclick = mostrarCarrito;
